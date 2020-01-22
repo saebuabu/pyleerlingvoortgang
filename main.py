5f495cbc02e6db7ca1aa62ae042a7e2be90be77b
asaebu <a.saebu@kw1c.nl>
2020-01-22 23:03:30 +0100

    Nu ook de leerlingresultaten in de juiste tabs per week op aparte rij

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from voortgangsdoc import SourceXls
 from rapportagedoc import TargetXls
+import constant
 
 mysrc = SourceXls()
 # De lijst van leerlingen wordt uit de tab gehaald met de naam van de klas
@@ -8,10 +9,8 @@ mysrc.initleerlingen()
 mysrc.actleerlingindex = -1
 # Alle leerlingen aflopen
 while mysrc.setnextleerling():
-    # actleerlingrij = mysrc.zoekleerlingrij()
-    # print("rij " + str(actleerlingrij) + " " + mysrc.namen[mysrc.actleerlingindex])
-
     firstvak = True
+    print(mysrc.namen[mysrc.actleerlingindex])
     # per leerling een aparte sheet maken of van een bestaande openen
     # vervolgens een nieuwe tab maken of die actief maken
     # De rij overzetten met zijn voortgang in de desbetreffende tab
@@ -22,27 +21,30 @@ while mysrc.setnextleerling():
     # ga dan vervolgens alle sheets na de actsheetindex af om de voortgamg op te zoeken
     hrange = []
     while mysrc.setnextsheet():
+        # leerlingvoortgangsdocument wordt geopend of aangemaakt
         if firstvak:
             voortgangLeerling = TargetXls(mysrc.klas, mysrc.namen[mysrc.actleerlingindex],
                                           mysrc.leerlingen[mysrc.actleerlingindex])
 
-        # maak een nieuwe tab na de laatste tab en maak het actief
-        # print(mysrc.sheet)
         else:
             print(".")
 
-        #print(mysrc.sheet.title)
+        # maak een nieuwe tab na de laatste tab en maak het actief
         voortgangLeerling.maaktab(mysrc.sheet.title)
 
         # kopieer de header range naar de leerlingvoortgangs document
-        hrange = mysrc.kopieerrange(1, 1, 40, 3)
-        # plak het in de leer
+        hrange = mysrc.kopieerrange(1, 1, constant.AANTALCOLSVOORTGANG, 3)
+        leerlingrij = mysrc.zoekleerlingrij()
+        leerlingrange = mysrc.kopieerrange(3, leerlingrij, constant.AANTALCOLSVOORTGANG, leerlingrij)
+        voortgangLeerling.pasteRange(1, 1, constant.AANTALCOLSVOORTGANG, 3, hrange, mysrc.sheet.title, True)
 
-        #voortgangLeerling.kopieerheader(mysrc.getheaderrange())
+        # plak de resultatenrij in de vrije rij met een volgend weeknummer
+        vrijerij = voortgangLeerling.zoekvrijerij(mysrc.sheet.title)
+        voortgangLeerling.pasteRange(3, vrijerij, constant.AANTALCOLSVOORTGANG, vrijerij, leerlingrange,
+                                     mysrc.sheet.title, False)
 
-        voortgangLeerling.pasteRange(1, 1, 40, 3, hrange, mysrc.sheet.title)
         hrange = []
-        #sla de aanpassingen op
+        # sla de aanpassingen op
         voortgangLeerling.save()
     firstvak = False
 
